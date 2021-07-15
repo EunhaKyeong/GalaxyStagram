78a154a20abbbd5c5c6c9cf11a26a86c47e1a1cc
EunhaKyeong <eh0634a@gmail.com>
2021-07-15 15:12:49 +0900

    구글 로그인 구현 완료

--- app/src/main/java/com/galaxy/galaxystagram/LoginActivity.kt
+++ app/src/main/java/com/galaxy/galaxystagram/LoginActivity.kt
@@ -7,7 +7,10 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import com.galaxy.galaxystagram.contract.SignInIntentContract
 import com.galaxy.galaxystagram.databinding.ActivityLoginBinding
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -21,15 +24,17 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         //getRoot 메서드로 레이아웃 내부의 최상위 위치 뷰의 인스턴스를 활용해 생성된 뷰를 액티비티에 표시.
         setContentView(binding.root)
-        
+
         auth = FirebaseAuth.getInstance()
 
+        //LoginActivity -> 구글로그인 화면 -> LoginActivity로 돌아온 후 콜백 함수.
+        //구글로그인 화면을 통해 얻어온 tokenId를 이용해 Firebase 사용자 인증 정보로 교환하고
+        //해당 정보를 사용해 Firebase에 인증합니다.
         launcher = registerForActivityResult(SignInIntentContract()) { result: String? ->
-                        result?.let {
-                            println("it -> ${it}")
-                            Toast.makeText(this, it, Toast.LENGTH_LONG).show()
-                        }
-                    }
+            result?.let {
+                firebaseAuthWithGoogle(it)  //tokenId를 이용해 firebase에 인증하는 함수 호출.
+            }
+        }
 
         binding.emailLoginButton.setOnClickListener {
             signUp()
@@ -39,6 +44,7 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    //회원가입
     fun signUp() {
         var email: String = binding.emailEditText.text.toString()
         var password: String = binding.passwordEditText.text.toString()
@@ -52,13 +58,17 @@ class LoginActivity : AppCompatActivity() {
                         getString(R.string.signin_complete), Toast.LENGTH_SHORT
                     ).show()
                     startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-                } else if (task.exception!!.toString().contains("FirebaseAuthWeakPasswordException")) {
+                } else if (task.exception!!.toString()
+                        .contains("FirebaseAuthWeakPasswordException")
+                ) {
                     //회원가입 시 비밀번호가 6자리 이상으로 입력하지 않은 경우
                     Toast.makeText(
                         this@LoginActivity, "비밀번호는 6자리 이상이어야 합니다.",
                         Toast.LENGTH_SHORT
                     ).show()
-                } else if (task.exception!!.toString().contains("FirebaseAuthUserCollisionException")) {
+                } else if (task.exception!!.toString()
+                        .contains("FirebaseAuthUserCollisionException")
+                ) {
                     //이미 존재하는 사용자 -> 로그인 함수 호출.
                     emailSignIn()
                 } else {
@@ -71,6 +81,7 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
+    //이메일 로그인
     fun emailSignIn() {
         var email: String = binding.emailEditText.text.toString()
         var password: String = binding.passwordEditText.text.toString()
@@ -96,8 +107,34 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
+    //구글 로그인
     fun googleSignIn() {
-
+        //Launcher를 실행해 LoginActivity -> 구글 로그인 화면으로 이동.
         launcher!!.launch(getString(R.string.default_web_client_id))
+    }
+
+    //tokenId를 이용해 firebase에 인증하는 함수.
+    fun firebaseAuthWithGoogle(idToken: String) {
+        //it가 tokenId, credential은 Firebase 사용자 인증 정보.
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+
+        //Firebase 사용자 인증 정보(credential)를 사용해 Firebase에 인증.
+        auth!!.signInWithCredential(credential)
+            .addOnCompleteListener(this@LoginActivity) { task: Task<AuthResult> ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(
+                        this@LoginActivity,
+                        getString(R.string.signin_complete),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    println("firebaseAuthWithGoogle => ${task.exception}")
+                    Toast.makeText(
+                        this@LoginActivity, getString(R.string.signin_google_faile),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/galaxy/galaxystagram/contract/SignInIntentContract.kt
+++ app/src/main/java/com/galaxy/galaxystagram/contract/SignInIntentContract.kt
@@ -8,15 +8,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.FirebaseAuth
 
-class SignInIntentContract: ActivityResultContract<String, String>() {
+class SignInIntentContract : ActivityResultContract<String, String>() {
     var googleSignInClient: GoogleSignInClient? = null
-    var auth: FirebaseAuth? = FirebaseAuth.getInstance()
-    var context: Context? = null
 
+    //LoginActivity에서 구글 아이디로 로그인 버튼을 누르면 구글 로그인 화면으로 이동하는 intent를 만드는 함수.
     override fun createIntent(context: Context, clientId: String): Intent {
-        this.context = context
         //구글로그인 초기설정
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(clientId)
@@ -30,37 +27,29 @@ class SignInIntentContract: ActivityResultContract<String, String>() {
         return signInIntent
     }
 
+    //구글 로그인 화면에서 다시 LoginActivity로 돌아올 때 호춯되는 함수.
     override fun parseResult(resultCode: Int, intent: Intent?): String? {
         return when (resultCode) {
+            //정상적으로 로그인이 이뤄진 경우 getTokenId 함수를 통해 얻은 tokenId를 LoginActivity에 전달.
             Activity.RESULT_OK -> getTokenId(intent)
             else -> null
         }
     }
 
-    fun getTokenId(data: Intent?):String? {
+    //tokenId를 리턴해주는 함수.
+    fun getTokenId(data: Intent?): String? {
         val task = GoogleSignIn.getSignedInAccountFromIntent(data)
         try {
             // Google Sign In was successful, authenticate with Firebase
             val account = task.getResult(ApiException::class.java)!!
-
-            return account.idToken!!
+            return account.idToken
         } catch (e: ApiException) {
             // Google Sign In failed, update UI appropriately
-            println("Google sign in failed=>${e}")
+            println("getTokenId() tokenId를 정상적으로 얻어오지 못함=>\n${e}")
             return null
         }
     }
 
-//    fun firebaseAuthWithGoogle(idToken: String) {
-//        val credential = GoogleAuthProvider.getCredential(idToken, null)
-//        auth!!.signInWithCredential(credential)
-//            .addOnCompleteListener(context) { task ->
-//                if (task.isSuccessful) {
-//                    // Sign in success, update UI with the signed-in user's information
-//                    val user = auth!!.currentUser
-//                } else {
-//                    // If sign in fails, display a message to the user.
-//                }
-//            }
-//    }
 }
+
+
