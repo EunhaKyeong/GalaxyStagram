197df909ad244b64fbe344a7e8858c3c8591bb8b
EunhaKyeong <eh0634a@gmail.com>
2021-07-22 00:36:14 +0900

    주석 추가

--- app/src/main/java/com/galaxy/galaxystagram/model/ContentDTO.kt
+++ app/src/main/java/com/galaxy/galaxystagram/model/ContentDTO.kt
@@ -1,14 +1,14 @@
 package com.galaxy.galaxystagram.model
 
-data class ContentDTO(var exaplain: String? = null,
-                      var imageUrl: String? = null,
-                      var uid: String? = null,
-                      var userEmail: String? = null,
-                      var timestamp: Long? = null,
-                      var favoriteCount: Int = 0,
-                      var favorites: Map<String, Boolean> = HashMap()) {
-    data class Comment(var uid: String? = null,
-                       var userEmail: String? = null,
-                       var comment: String? = null,
-                       var timestamp: Long? = null)
+data class ContentDTO(var exaplain: String? = null,     //게시글
+                      var imageUrl: String? = null,     //이미지 url
+                      var uid: String? = null,      //글 작성자 식별자
+                      var userEmail: String? = null,    //글 작성자 이메일
+                      var timestamp: Long? = null,      //작성일
+                      var favoriteCount: Int = 0,       //좋아요 수
+                      var favorites: Map<String, Boolean> = HashMap()) {    //좋아요를 누른 사람들
+    data class Comment(var uid: String? = null,     //댓글 작성자 식별자
+                       var userEmail: String? = null,   //댓글 작성자 이메일
+                       var comment: String? = null,     //댓글
+                       var timestamp: Long? = null)     //댓글 작성일
 }
