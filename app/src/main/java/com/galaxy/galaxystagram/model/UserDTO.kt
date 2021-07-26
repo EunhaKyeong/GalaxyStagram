a3d788168fbe1407e9b545cf9d0d6cd656b118c7
EunhaKyeong <eh0634a@gmail.com>
2021-07-26 16:07:34 +0900

    팔로잉, 팔로워 변수명 변경

--- app/src/main/java/com/galaxy/galaxystagram/model/UserDTO.kt
+++ app/src/main/java/com/galaxy/galaxystagram/model/UserDTO.kt
@@ -1,7 +1,7 @@
 package com.galaxy.galaxystagram.model
 
 data class UserDTO(var email: String? = null,
-                var profileImgUrl: String? = null,
-                var explain: String? = null,
-                var Following: MutableMap<String, Boolean> = HashMap(),
-                var Follwer: MutableMap<String, Boolean> = HashMap())
+                   var profileImgUrl: String? = null,
+                   var explain: String? = null,
+                   var following: MutableMap<String, Boolean> = HashMap(),
+                   var follower: MutableMap<String, Boolean> = HashMap())
